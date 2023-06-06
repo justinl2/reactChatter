8064b842d568363bd964fdb31bf857d423064742
Justin Li <justinli@Justins-MacBook-Pro.local>
2023-06-06 07:57:49 -0400

    refactor

--- App.tsx
+++ App.tsx
@@ -4,10 +4,7 @@ import {
   View,
 } from 'react-native';
 import Navigation from './src/navigation';
-import { Amplify } from 'aws-amplify';
-import awsconfig from './src/aws-exports';
 
-Amplify.configure(awsconfig);
 
 export default function App() {
   return (
